38355a18d74f990e58aca04eeda4f6540e9affb3
Rihards Kauliņš <rkaulins2@edu.riga.lv>
2020-02-19 15:24:15 +0200

    estructuring

--- public_html/index.js
+++
@@ -1,7 +0,0 @@
-let text = document.getElementById("sample").innerHTML;
-let textArr = text.split(" ");
-let endStr = "";
-textArr.forEach(element => {
-    endStr += "<span class=\"term-text\">" + element + "</span> ";
-});
-document.getElementById("sample").innerHTML = endStr;
